c5964c1eda71f5579cfb90025fbf2a6863b36982
anilinfinity1678 <94840674+anilinfinity1678@users.noreply.github.com>
2023-05-16 16:40:26 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
-import Login from "./Login/Login";
-import React from "react";
-import Registration from "./Registration/Registration";
-import Home from "./Home/Home";
-import ILETS from "./MainApp/Categories/ILETS/ILETS";
-import Job from "./MainApp/Categories/JOB/Job";
-import Mock from "./MainApp/Categories/MOCKIIM/Mock";
-import Main from "./Main/Main";
-import CatSelction from "./MainApp/CategorySelction/CatSelction";
-import Profile from "./MainApp/Profile/Profile";
-import { Routes, Route } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-        <>
-            <Routes>
-                <Route path="/" element={<Home />}></Route>
-                <Route path="/ilets" element={<ILETS />}></Route>
-                <Route path="/job" element={<Job />}></Route>
-                <Route path="/mockiim" element={<Mock />}></Route>
-                <Route
-                    path="/login/registration"
-                    element={<Registration />}
-                ></Route>
-                <Route path="/category" element={<CatSelction />} />
-                <Route path="/login" element={<Login />}></Route>
-                <Route path="/main" element={<Main />}></Route>
-                <Route path="/profile" element={<Profile />}></Route>
-            </Routes>
-        </>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
